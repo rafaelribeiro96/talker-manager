da5da9734ad68b5f9a71f0938c71596c2c078bb5
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-11-07 15:41:36 -0300

    requisito 4 realizado

---
+++ src/middlewares/validateLogin.js
@@ -0,0 +1,27 @@
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (!email || email.length === 0) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  } 
+  const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  } 
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+module.exports = {
+  validateEmail,
+  validatePassword,
+};
--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const tokenGenerator = require('../utils/tokenGenerator');
+const { validateEmail, validatePassword } = require('../middlewares/validateLogin');
 
 const router = express();
 
-router.post('/', (_req, res) => {
+router.post('/',
+  validateEmail,
+  validatePassword,
+   (_req, res) => {
   const token = tokenGenerator();
   res.status(200).json({ token });
 });
